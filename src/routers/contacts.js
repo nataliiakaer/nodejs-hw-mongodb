80bba8538e4f8eead835736b42a613de09f202c8
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2024-10-17 23:21:16 +0300

    start adding validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -10,6 +10,7 @@ import {
   upsertContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 const jsonParser = express.json({
@@ -18,20 +19,30 @@ const jsonParser = express.json({
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
 
 router.post('/contacts', jsonParser, ctrlWrapper(createContactController));
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
 router.put(
   '/contacts/:contactId',
+  isValidId,
   jsonParser,
   ctrlWrapper(upsertContactController),
 );
 
 router.patch(
   '/contacts/:contactId',
+  isValidId,
   jsonParser,
   ctrlWrapper(patchContactController),
 );

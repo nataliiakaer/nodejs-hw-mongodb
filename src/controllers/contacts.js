f17ce0948925a7506d3a9d35e8ed1b3baa520704
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2024-10-22 22:25:58 +0300

    added filter by contact type

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -8,12 +8,14 @@ import {
 import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export const getContactsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
   const { sortOrder, sortBy } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
 
-  const contacts = await getAllContacts({ page, perPage, sortBy, sortOrder });
+  const contacts = await getAllContacts({ page, perPage, sortBy, sortOrder, filter });
 
   res.status(200).json({
     status: 200,
---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,31 @@
+// src/utils/parseFilterParams.js
+
+const parseType = (contactType) => {
+  const isString = typeof contactType === 'string';
+  if (!isString) return;
+  const isType = (contactType) => ['work', 'home', 'personal'].includes(contactType);
+
+  if (isType(contactType)) return contactType;
+};
+
+// const parseIsFavourite = (boolean) => {
+//   const isString = typeof number === 'string';
+//   if (!isString) return;
+
+//   const parsedNumber = parseInt(number);
+//   if (Number.isNaN(parsedNumber)) {
+//     return;
+//   }
+
+//   return parsedNumber;
+// };
+
+export const parseFilterParams = (query) => {
+  const { contactType } = query;
+
+  const parsedType = parseType(contactType);
+
+  return {
+    contactType: parsedType,
+  };
+};
